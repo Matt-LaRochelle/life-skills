307236852481684cd2ed564aa01350341f175f57
matthew <larochelle.matthew@gmail.com>
2023-10-05 12:39:25 -0700

    Add date and done elements

--- frontend/src/pages/skillList/SkillList.js
+++ frontend/src/pages/skillList/SkillList.js
@@ -1,27 +1,32 @@
 // import { useEffect } from 'react'
 import { useAuthContext } from '../../hooks/useAuthContext'
+import { format } from 'date-fns';
 
 import './skillList.css'
 
 const SkillList = () => {
 
     const {user} = useAuthContext()
+    const currentDate = new Date();
+    const formattedDate = format(currentDate, 'EEEE, MMMM do, y');
+    console.log(formattedDate);
 
 
     return (
         <div className="skillList__container">
             <h2>Life Skills:</h2> 
+            <p>{formattedDate}</p>
             <ul>
-                <li>Exercise consistently</li>
-                <li>Personal finance</li>
-                <li>Meditation</li>
-                <li>Communication</li>
-                <li>Waking up early</li>
-                <li>Public speaking</li>
-                <li>Getting honest with yourself</li>
-                <li>Leadership skills</li>
-                <li>Decision making</li>
-                <li>Listening</li>
+                <li><span>Done</span>Exercise consistently</li>
+                <li><span>Done</span>Personal finance</li>
+                <li><span>Done</span>Meditation</li>
+                <li><span>Done</span>Communication</li>
+                <li><span>Done</span>Waking up early</li>
+                <li><span>Done</span>Public speaking</li>
+                <li><span>Done</span>Getting honest with yourself</li>
+                <li><span>Done</span>Leadership skills</li>
+                <li><span>Done</span>Decision making</li>
+                <li><span>Done</span>Listening</li>
             </ul>
         </div>
     )
