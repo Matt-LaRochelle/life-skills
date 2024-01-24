7b8d460615869d9529b0a26ec6466c6d928a68a9
matthew <larochelle.matthew@gmail.com>
2024-01-23 17:17:59 -0800

    finish login component

--- frontend/src/pages/login/Login.js
+++ frontend/src/pages/login/Login.js
@@ -21,41 +21,48 @@ const Login = () => {
                 <div className="color"></div>
                 <div className="color"></div>
                 <div className="color"></div>
-            <div className="container">
-            <form className="form" onSubmit={handleSubmit}>
-                <h2>Log In</h2>
-                <div className="inputBox">
-                    <input
-                        type="email"
-                        placeholder='Email'
-                        onChange={(e) => setEmail(e.target.value)}
-                        value={email}
-                    />
-                </div>
-                <div className='inputBox'>
-                    <input
-                        type="password"
-                        placeholder='Password'
-                        onChange={(e) => setPassword(e.target.value)}
-                        value={password}
-                    />
-                </div>
-                <div className='inputBox'>
-                    <input type="submit" value="Login" disabled={isLoading} />
-                </div>
+                <div className='box'>
+                    <div className='square' style={{ '--i': 0 }}></div>
+                    <div className='square' style={{ '--i': 1 }}></div>
+                    <div className='square' style={{ '--i': 2 }}></div>
+                    <div className='square' style={{ '--i': 3 }}></div>
+                    <div className='square' style={{ '--i': 4 }}></div>
+                    <div className="container">
+                        <form className="form" onSubmit={handleSubmit}>
+                            <h2>Log In</h2>
+                            <div className="inputBox">
+                                <input
+                                    type="email"
+                                    placeholder='Email'
+                                    onChange={(e) => setEmail(e.target.value)}
+                                    value={email}
+                                />
+                            </div>
+                            <div className='inputBox'>
+                                <input
+                                    type="password"
+                                    placeholder='Password'
+                                    onChange={(e) => setPassword(e.target.value)}
+                                    value={password}
+                                />
+                            </div>
+                            <div className='inputBox'>
+                                <input type="submit" value="Login" disabled={isLoading} />
+                            </div>
 
-                <p className='forget'>Forgot your password? <Link to="/forgot">Reset it here</Link></p>
-                <p className='forget'>Don't have an account? <Link to="/signup">Signup here</Link></p>
-                {error && <div className='error'>{error}</div>}
-                {isLoading && 
-                        <div className="loading">
-                            <p>Fetching data from server...</p>
-                            <p>This process tends to take 5-60 seconds</p>
-                            <ClimbingBoxLoader color="#36d7b7" />
-                        </div>}
-            </form>
-            </div>
+                            <p className='forget'>Forgot your password? <Link to="/forgot">Reset it here</Link></p>
+                            <p className='forget'>Don't have an account? <Link to="/signup">Signup here</Link></p>
+                            {error && <div className='error'>{error}</div>}
+                            {isLoading && 
+                                    <div className="loading">
+                                        <p>Fetching data from server...</p>
+                                        <p>This process tends to take 5-60 seconds</p>
+                                        <ClimbingBoxLoader color="#36d7b7" />
+                                    </div>}
+                        </form>
+                    </div>
             
+                </div>
             </section>
         </div>
     )
